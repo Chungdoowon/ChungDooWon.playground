6d72520eb74f3a12913533ccc9dac60c9d4d6070
Chungdoowon <ccomi95@korea.ac.kr>
2016-08-08 14:09:38 +0900

    Do

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+typealias Midterm_exam = (korea:Int, math:Int, english:Int)
+
+//100점 만점이라 가정
+
+let JeMin:Midterm_exam =    (78,45,67)
+let SuJin:Midterm_exam =    (75,65,45)
+let DongHo:Midterm_exam =   (78,68,60)
+let JuChan:Midterm_exam =   (65,90,88)
+let HeMi:Midterm_exam =     (47,45,65)
+let HeeCkeul:Midterm_exam = (12,70,67)
+let DooWon:Midterm_exam =   (96,92,88)
+let KangJu:Midterm_exam =   (17,30,65)
+let SangGi:Midterm_exam =   (35,35,20)
+let MounHo:Midterm_exam =   (44,52,76)
+
+let score = ("A","B","C","D")
+
+// 국어점수가 가장 높은 사람의 이름은?
+// 수학점수가 가장 낮은 사람의 이름은?
+// 평균점수가 전체 평균보다 높은 사람들의 이름은?
+// 평균 90이상의 경우 A학점이라고 할때 누가 A를 받을까?
+// 각 과목 별 가장 낮은 점수의 학생과 점수를 나열하시오.
